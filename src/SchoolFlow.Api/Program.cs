2940fa570f6cf502a40a6e7b8897387e8d53c321
Heidy Kengne <kengeheidy2016@gmail.com>
2026-03-04 11:56:34 +0100

    relaunch

--- src/SchoolFlow.Api/Program.cs
+++ src/SchoolFlow.Api/Program.cs
@@ -219,6 +219,12 @@ app.UseSwaggerUI(options =>
     options.DocumentTitle = "SchoolFlow API - Documentation";
 });
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); // HTTP uniquement
+});
+
+
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 
